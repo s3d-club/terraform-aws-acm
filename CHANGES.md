0ccfccbdd669e413660d205561232dd715402936
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-25 13:50:59 +0000

    0.1.11 Renamed `change` to `name` module
    - Renamed `change` to `name` module

--- CHANGES.md
+++ CHANGES.md
@@ -4,10 +4,9 @@ Recently completed and planned work is tracked here.
 Plan:
 - **TODO** remove upstream code and re-implement with a focus on simplicity
 
-## [0.0.0](.) to [0.1.9](.)
+## [0.0.0](.) to [0.1.10](.)
 - Created our early versions
 - Forked upstream code
 
-## [0.1.10](.)
-- Updated `.terraform.lock.hcl`
-- Updated `change` module
+## [0.1.11](.)
+- Renamed `change` to `name` module
--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "change" {
+module "name" {
   source = "github.com/s3d-club/terraform-external-name?ref=v0.1.9"
 
   path = path.module
@@ -9,7 +9,7 @@ module "upstream" {
   source = "./upstream"
 
   domain_name               = var.domain
-  tags                      = module.change.tags
+  tags                      = module.name.tags
   zone_id                   = var.zone_id
   subject_alternative_names = var.subject_alternative_names
 }
